83f37298bea4a97d2fd55c5b6f5902e8c12a226a
LexaBog <lexabogit@gmail.com>
2025-01-16 19:13:05 +0100

    пробую мидлвер

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
-function App() {
-  const [character, setCharacter] = useState(null);
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import CharacterInfo from "./components/CharacterInfo.js";
+import Game from "./components/Game.js";
+import "./App.css";
 
-  const handleCharacterLoaded = (data) => {
-      setCharacter(data); // Сохраняем данные персонажа
-  };
+function App({ telegramId }) {
+    const [character, setCharacter] = useState(null);
 
-  return (
-      <div className="ollGameBody">
-          <div>
-              <h1 className="header">Dungeons & Heroes</h1>
-          </div>
+    const handleCharacterLoaded = (data) => {
+        setCharacter(data); // Сохраняем данные персонажа
+    };
 
-          {/* Передача данных через маршруты */}
-          <Routes>
-              <Route
-                  path="/"
-                  element={<CharacterInfo onCharacterLoaded={handleCharacterLoaded} />}
-              />
-              <Route
-                  path="/game"
-                  element={<Game character={character} />}
-              />
-          </Routes>
+    return (
+        <div className="ollGameBody">
+            <div>
+                <h1 className="header">Dungeons s Heroes</h1>
+            </div>
 
-          <div className="futer"></div>
-      </div>
-  );
+            {/* Передача данных через маршруты */}
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <CharacterInfo
+                            telegramId={telegramId}
+                            onCharacterLoaded={handleCharacterLoaded}
+                        />
+                    }
+                />
+                <Route
+                    path="/game"
+                    element={<Game character={character} />}
+                />
+            </Routes>
+
+            <div className="futer"></div>
+        </div>
+    );
 }
+
+export default App;
