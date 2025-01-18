6c8504dd147ff5b641e01ceb77547a99772b524f
LexaBog <lexabogit@gmail.com>
2025-01-18 22:02:39 +0100

    упростил код убрал безопасность

--- src/components/authService.js
+++ src/components/authService.js
@@ -5,8 +5,8 @@ export const authenticateUser = async (telegramId, username) => {
   try {
     // const user = await authenticateUser(telegramId, username);
 
-    const response = await axios.post("https://dangeon-db-beck.onrender.com/api/auth", { telegramId, username });
-    // const response = await axios.post("http://localhost:5021/api/auth", { telegramId, username });
+    // const response = await axios.post("https://dangeon-db-beck.onrender.com/api/auth", { telegramId, username });
+    const response = await axios.post("http://localhost:5021/api/auth", { telegramId, username });
     console.log("Данные пользователя:", username);
     return response.data.user;
   } catch (error) {
@@ -18,8 +18,8 @@ export const authenticateUser = async (telegramId, username) => {
 // Получение данных персонажа
 export const fetchCharacter = async (telegramId) => {
   try {
-    const response = await axios.post("https://dangeon-db-beck.onrender.com/api/characters", { telegramId });
-    // const response = await axios.post("http://localhost:5021/api/characters", { telegramId });
+    // const response = await axios.post("https://dangeon-db-beck.onrender.com/api/characters", { telegramId });
+    const response = await axios.post("http://localhost:5021/api/characters", { telegramId });
     return response.data;
   } catch (error) {
     console.error("Ошибка при запросе персонажа:", error.response?.data || error.message);
