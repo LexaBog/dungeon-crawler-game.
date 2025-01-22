56c058b0be05a20f4f63f56897bd446a9f2711c3
LexaBog <lexabogit@gmail.com>
2025-01-22 19:38:31 +0100

    в

--- src/components/EnergyStatus.js
+++ src/components/EnergyStatus.js
@@ -48,11 +48,26 @@ const EnergyStatus = ({ characterId, setCharacterId }) => {
     saveUpdates({ health: newHealth });
   };
 
+  const useExperiencePotion = () => {
+    const experienceFromPotion = 50; // Опыт, полученный от зелья
+    updateCharacter(telegramId, { experience: experienceFromPotion })
+      .then((updatedCharacter) => {
+        console.log("Персонаж обновлён после использования зелья:", updatedCharacter);
+        setCharacter(updatedCharacter);
+      })
+      .catch((error) => {
+        console.error("Ошибка обновления персонажа после использования зелья:", error);
+      });
+  };
+  
   return (
     <div className="energy-status">
       <ProgressBar label="Здоровье" value={characterId.health} max={characterId.maxHealth} color="#b22222" />
       <ProgressBar label="Мана" value={characterId.mana} max={characterId.maxMana} color="blue" />
       <button onClick={useHealthPotion} className="use-potion-button">
+        Использовать зелье здоровья
+      </button>
+      <button onClick={useExperiencePotion} className="use-potion-button">
         Использовать зелье здоровья
       </button>
     </div>
