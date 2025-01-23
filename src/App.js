a393a21da2175907b483e996f8c4460b922a4e09
LexaBog <lexabogit@gmail.com>
2025-01-23 18:59:57 +0100

    d

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import CharacterInfo from "./components/CharacterInfo.js";
-import Header from './components/Header.js';
 import Game from "./components/Game.js";
-import { authenticateUser, validateToken,} from "./components/authService.js";
+import Header from "./components/Header.js";
+import { authenticateUser, validateToken } from "./components/authService.js";
 import "./App.css";
 
 function App() {
@@ -13,19 +13,24 @@ function App() {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    // Получение токена из URL или localStorage
+    const storedToken = localStorage.getItem("token");
     const urlParams = new URLSearchParams(window.location.search);
-    const token = urlParams.get("token");
-    // console.log("Токен из URL:", token);
+    const token = urlParams.get("token") || storedToken;
 
     if (!token) {
       setError("Токен отсутствует. Перейдите по ссылке от бота.");
       return;
     }
 
+    // Сохраняем токен в localStorage, если он пришёл из URL
+    if (!storedToken && token) {
+      localStorage.setItem("token", token);
+    }
+
     // Валидация токена
     validateToken(token)
       .then(({ telegramId, username }) => {
-        // console.log("Валидный токен. Данные пользователя:", { telegramId, username });
         setTelegramId(telegramId);
         setUsername(username);
 
@@ -33,7 +38,6 @@ function App() {
         return authenticateUser(telegramId, username);
       })
       .then((user) => {
-        // console.log("Пользователь успешно авторизован:", user);
         setCharacterId(user.characterId); // Сохраняем characterId для использования
       })
       .catch((err) => {
@@ -41,7 +45,6 @@ function App() {
         setError("Не удалось авторизовать пользователя.");
       });
   }, []); // Пустой массив зависимостей
-  
 
   // Отображение ошибок или загрузки
   if (error) return <p>{error}</p>;
