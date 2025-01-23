787e5390fcd0eeebd9bf4f799fc2c0423d07d8a0
LexaBog <lexabogit@gmail.com>
2025-01-23 22:03:15 +0100

    f

--- src/components/navigationButtons/NavigationButtons .js
+++ src/components/navigationButtons/NavigationButtons .js
@@ -8,23 +8,17 @@ const NavigationButtons  = () => {
     const navigate = useNavigate();
     
     return (
-        <>
-            {/* <div className="boxNameUndLavel">
-                <p>{characterId.name}</p>
-                <p className="texStyleHeader">{characterId.level} level </p>
-                <p className="texStyleHeader"> Опыт: {characterId.experience}</p>
-            </div> */}
-          
-            <button className="navigate-button" onClick={() => navigate("/")}>
+        <div className="navigate-button-box">        
+            <button className="" onClick={() => navigate("/")}>
                 Прокачка
             </button>
-            <button className="navigate-button" onClick={() => navigate("/character")}>
+            <button className="" onClick={() => navigate("/character")}>
                 <img  className="button-Haracteristic" src="/button-Haracteristic.webp" alt="Character" />    
             </button>
-            <button className="navigate-button" onClick={() => navigate("/")}>
+            <button className="" onClick={() => navigate("/")}>
                 Магазик
             </button>
-        </>
+        </div>
     );
 };
 
